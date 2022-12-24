088caa060ae2e7c883bb336cd59723524065a8ab
jbabbo2 <jbabbo2@illinois.edu>
2022-12-24 14:56:23 -0600

    placeholder title

--- app/[institutionSlug]/course/[courseId]/layout.tsx
+++ app/[institutionSlug]/course/[courseId]/layout.tsx
@@ -13,22 +13,11 @@ export default function CourseIdLayout({
   return (
     <div className="flex h-screen">
       {/* Desktop */}
-    <Transition
-      appear={true}
-      show={true}
-      enter="transition-opacity duration-[100ms]"
-      enterFrom="opacity-0"
-      enterTo="opacity-100"
-      leave="transition-opacity duration-150"
-      leaveFrom="opacity-100"
-      leaveTo="opacity-0"
-    >
       <div className="flex h-screen py-2 bg-zinc-800">
         <div className="hidden overflow-y-auto bg-zinc-800 outline-1 md:block md:w-64 border-l border-zinc-700 px-2">
           <SecondayContent />
         </div>
       </div>
-    </Transition>
 
       <div className="relative grow flex flex-col h-screen">
         <div className="flex-auto">{children}</div>
--- components/column/secondary-column/SecondayContentWIP.tsx
+++ components/column/secondary-column/SecondayContentWIP.tsx
@@ -123,7 +123,10 @@ export const SecondayContent = () => {
       <div className="flex flex-1 flex-col overflow-y-auto pt-0 pb-4">
         <div className="flex flex-shrink-0 items-center px-3"></div>
         <div className="flex flex-col flex-1">
-          <ColumnNavSection content={content} spaceTop={true} />
+          <p className='text-gray-300 group w-full flex items-center px-2 pt-6 pb-7 text-left text-xl font-bold rounded-xl truncate'>
+            Course Name Here
+          </p>
+          <ColumnNavSection content={content} />
         </div>
       </div>
     </div>
