3cc0461340cfe35fd7d3be88fa043e3f19dc6a5a
jbabbo2 <jbabbo2@illinois.edu>
2022-12-28 16:22:22 -0600

    clean up remove log

--- api/axios.tsx
+++ api/axios.tsx
@@ -3,7 +3,6 @@
 import axios from "axios";
 
 const cachedUser = JSON.parse((typeof window !== 'undefined') && localStorage.getItem('user') || "null");
-console.log(process.env.NEXT_PUBLIC_RAILS_BASE_URL)
 export const axiosAuth = axios.create({
   baseURL: process.env.NEXT_PUBLIC_RAILS_BASE_URL,
   headers: { Authorization: `Bearer ${cachedUser?.access_token ?? ''}` }
