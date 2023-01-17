a5db3a8b100654fec0bff87261be1e6481e3b763
Tony Jiang <86689874+hippopotony@users.noreply.github.com>
2023-01-17 00:34:27 -0800

    Now upload works for syllabus

--- app/[institutionSlug]/course/[courseId]/syllabus/layout.tsx
+++ app/[institutionSlug]/course/[courseId]/syllabus/layout.tsx
@@ -4,7 +4,7 @@ export default function SyllabusLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="max-w-4xl max-h-64 bg-zinc-400 resize rounded-lg">
+    <div>
       {children}
     </div>
   )
