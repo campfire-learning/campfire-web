7581af9e46dfca4aa37d4431bbf39622739b708c
Tony Jiang <86689874+hippopotony@users.noreply.github.com>
2023-01-29 00:13:57 -0800

    Got it - assignments work (mostly)

--- app/[institutionSlug]/course/[courseId]/assignment/[assignmentId]/page.tsx
+++ app/[institutionSlug]/course/[courseId]/assignment/[assignmentId]/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { AssignmentEdit } from "components/column/secondary-column/AssignmentEdit";
+
 export default function AssignmentIdPage() {
+  const currentPath = usePathname();
+  // const courseId: string = currentPath.split("/")[3];
+  const assignmentId: string = currentPath.split("/")[5];
+
   return (
-    <>
-      <h1>Ho Ho Ho</h1>
-    </>
+    <AssignmentEdit assignmentId={assignmentId} assignmentType="assignment" />
   )
 }
